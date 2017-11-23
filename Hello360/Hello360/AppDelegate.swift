47e36110d23dc81f077d55fd54f3d5bc3b2fb9a6
Jan Chaloupecky <jan.chaloupecky@360dialog.com>
2017-11-23 16:25:33 +0100

    typos

--- Hello360/Hello360/AppDelegate.swift
+++ Hello360/Hello360/AppDelegate.swift
@@ -1,15 +1,10 @@
 //
-//  AppDelegate.swift
-//  Hello360
-//
-//  Created by Jan Chaloupecky on 14.11.17.
-//  Copyright © 2017 360dialog. All rights reserved.
+//  This file is part of 360 Dialog SDK.
+//  See the file LICENSE.txt for copying permission.
 //
 
 import UIKit
-
 import D360Kit
-
 import UserNotifications
 
 @UIApplicationMain
@@ -22,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Those are sample credentials for test purpose. You can get your by loging in to https://dashboard.360dialog.com/apps/
         let options = D360Options(appId: "213", apiKey: "8e82138de17f708c544173ae1463a6f61eb7133aaec80113c7941e26ce0dfeed")
         
-        // You can customise furhter the SDK using the option object.
+        // You can customise further the SDK using the option object.
         options.sessionBackgroundTimeout = 60
         
         D360.start(withOptions: options)
@@ -37,7 +32,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     
-    //MARK: - Rmote Notification Token
+    //MARK: - Remote Notification Token
     
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         // Hand over the push token
@@ -53,7 +48,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         D360.push().registerNotificationCategories(notificationSettings.categories)
     }
     
-    // MARK: - Rmote Notification Paylod
+    // MARK: - Remote Notification Payload
     
     func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
         
@@ -61,7 +56,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 }
 
-// MARK: User Notifications
+// MARK: - User Notifications
 @available(iOS 10, *)
 extension AppDelegate: UNUserNotificationCenterDelegate {
     
@@ -77,7 +72,7 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
     }
 }
 
-// MARK: Style
+// MARK: - Style
 extension AppDelegate {
     func applyStyle() {
         
